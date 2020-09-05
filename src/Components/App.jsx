82f2dfc5f43f27d4f88a8bbb203edd7e058655a6
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-06 00:32:26 +0300

    Added server requests and breakdown by component

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,24 +1,62 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import { GlobalStyle } from '../config/preparation';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from 'react';
+import Axios from "axios";
 
-const Title = styled.h1`
-    color: red;
-    text-align: center;
-`
+import { Context } from '../hooks/context';
+import { GlobalStyle } from '../styles/preparation';
+import { Error } from './Error/Error';
+import { Search } from './Search/Search';
+import { Blog } from './Blog/Blog';
 
-const App = () => {
-    const [title, setTitle] = useState('Hello!');
 
-    return (
-        <>
-        <GlobalStyle />
-        <div>
-            <Title>{title}</Title>
-        </div>
-        </>
+export const App = () => {
+    const [posts, setPosts] = useState([]);
+    const [users, setUsers] = useState([]);
+    const [postsError, setPostsError] = useState(false);
+    const [usersError, setUsersError] = useState(false);
 
-    );
+    const axios = Axios.create();
+    const url = "https://jsonplaceholder.typicode.com"
+    const header = {
+        header: {'Access-Control-Allow-Origin': 'https://jsonplaceholder.typicode.com/',}
+    }
+
+
+    useEffect(() => {
+        axios
+        .get(`${url}/posts`, header)
+        .then((response) => {
+            setPosts(response.data);
+            getUsers();
+        })
+        .catch((error) => {
+            error.response || error.request ? setPostsError(true) : setPostsError(false)
+        })
+    },[]);
+
+    const getUsers = () => {
+        axios
+        .get(`${url}/users`, header)
+        .then((response) => {
+            setUsers(response.data);
+        })
+        .catch((error) => {
+            error.response || error.request ? setUsersError(true) : setUsersError(false)
+        })
+    }
+    
+
+    if (!postsError && !usersError) {
+        return (
+            <Context.Provider value={{ posts, users }}>
+                <>
+                <GlobalStyle />
+                    <Search />
+                    <Blog />
+                </>
+            </Context.Provider>
+        );
+    } else {
+        return <Error />
+    }
 }
-
-export default App;
