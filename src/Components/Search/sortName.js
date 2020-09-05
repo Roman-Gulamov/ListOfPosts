f82e163b99b4224089c9a007ff7004c60e49e6ab
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-06 00:34:38 +0300

    Search by text

---
+++ src/Components/Search/sortName.js
@@ -0,0 +1,18 @@
+import { sortRef } from '../../hooks/ref';
+
+export const sortName = (event) => {
+    const sort = sortRef.current;
+    const inputValue = event.target.value;
+    const iterableUsers = [...sort.children];
+
+    if (inputValue !== '') {
+        iterableUsers.forEach((child) => {
+            const search = child.innerText.search(RegExp(inputValue, "gi"));
+            return search === -1 ? child.style.display = "none" : child.style.display = "block";
+        });
+    } else {
+        iterableUsers.forEach((child) => {
+            child.style.display = "block";
+        });
+    }
+}
