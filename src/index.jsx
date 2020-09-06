f7a184ae4c1aa3d3ff4a43bc2a6cd7bf2f66741a
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-06 23:29:20 +0300

    Main file

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { App } from './Components/App';
+import './assets/fonts/Long Cang/stylesheet.css';
+
+
+ReactDOM.render(<App />,document.getElementById('root'));
