be1d4cb68e74a9fb1daf0d65ff2b03b13b30cd16
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-06 23:29:08 +0300

    Function defining avatars

---
+++ src/Components/Blog/findUserImg.js
@@ -0,0 +1,39 @@
+import Leanne from '../../assets/images/Leanne.png';
+import Ervin from '../../assets/images/Ervin.png';
+import Clementine from '../../assets/images/Clementine.png';
+import Patricia from '../../assets/images/Patricia.png';
+import Chelsey from '../../assets/images/Chelsey.png';
+import Dennis from '../../assets/images/Dennis.png';
+import Kurtis from '../../assets/images/Kurtis.png';
+import Nicholas from '../../assets/images/Nicholas.png';
+import Glenna from '../../assets/images/Glenna.png';
+import Clementina from '../../assets/images/Clementina.png';
+import Anonim from '../../assets/images/Anonim.png';
+
+
+export const findUserImg = (id) => {
+    switch (id) {
+        case 1:
+            return Leanne
+        case 2:
+            return Ervin
+        case 3:
+            return Clementine
+        case 4:
+            return Patricia
+        case 5:
+            return Chelsey
+        case 6:
+            return Dennis
+        case 7:
+            return Kurtis
+        case 8:
+            return Nicholas
+        case 9:
+            return Glenna
+        case 10:
+            return Clementina
+        default:
+            return Anonim
+    }
+}
