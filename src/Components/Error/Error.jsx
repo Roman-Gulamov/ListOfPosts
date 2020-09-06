3261a54e78b5799f96880b4e6618e7e924947e09
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-06 23:28:32 +0300

    Updated markup for the Error component

--- src/Components/Error/Error.jsx
+++ src/Components/Error/Error.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { ErrorMes } from '../../styles/error';
+import { ErrorMes, ErrorMesText } from '../../styles/error';
 
 export const Error = () => {
     return (
         <ErrorMes className="result">
-            <span role="img" aria-label="cry">&#128547;</span>
-                Ошибка при загрузки
-            <span role="img" aria-label="cry">&#128547;</span>
+            <ErrorMesText>
+                Error connecting to server...
+            </ErrorMesText>
         </ErrorMes>
     )
 }
