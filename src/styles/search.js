cb856b218f36220d9830ff076d3130f991f8a3d3
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-06 23:29:57 +0300

    Styles for the Header component

--- src/styles/search.js
+++ src/styles/search.js
@@ -1,7 +1,39 @@
 import styled from 'styled-components';
 
+export const Header = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #00acee;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    height: 50px;
+    padding: 0 70px;
+`
 
-export const Header = styled.div`
+export const HeaderIcon = styled.div`
+    color: white;
+    margin-right: 10px;
+`
 
+export const HeaderSearch = styled.div`
+    color: white;
+    width: 90%;
+    height: 40px;
+`
+
+export const SearchInput = styled.input`
+    width: 100%;
+    height: 100%;
+    border: 4px solid #00acee;
+    padding: 5px 10px;
+    border-radius: 10px;
+    transition: border-color 0.4s ease;
+
+    &:focus {
+        outline: none;
+        border-color: #0d8fc2;
+    }
 `
 
