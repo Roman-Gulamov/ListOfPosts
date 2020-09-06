3ac8dc2235530424fac491c5fb8f624086993e16
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-06 23:25:53 +0300

    Styles for error

--- src/styles/error.js
+++ src/styles/error.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 
 export const ErrorMes = styled.div`
-    
+    display: flex;
+    align-items: center;
+    height: 100vh;
+`
+
+export const ErrorMesText = styled.p`
+    margin: auto;
+    font-size: 100px;
 `
