3c5d751072b35ef4869c89cf9060b0c66a1f7db1
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-06 23:28:46 +0300

    Updated markup for the Header component

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -4,21 +4,23 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 import { sortName } from './sortName';
-import { Header } from '../../styles/search';
+import { Header, HeaderIcon, HeaderSearch, SearchInput } from '../../styles/search';
 
 
 export const Search = () => {
     return (
         <Header>
-            <div>
-                <FontAwesomeIcon icon={faTwitter} size="lg" />
-            </div>
-            <input
-                type="text" 
-                className="header-search"
-                onInput={(event) => sortName(event)} 
-                placeholder="Search..." 
-            />
+            <HeaderIcon>
+                <FontAwesomeIcon icon={faTwitter} size="2x" />
+            </HeaderIcon>
+            <HeaderSearch>
+                <SearchInput
+                    type="text" 
+                    className="header-search"
+                    onInput={(event) => sortName(event)} 
+                    placeholder="Search..." 
+                />
+            </HeaderSearch>
         </Header>
     )
 }
